e6c247eb83deb2364df6d2e5a66954745d772033
gerard_soumabere <gerard.soumabere@gmail.com>
2024-04-29 15:13:38 +0200

    Site schema + .gitignore

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>titre</title>
+    <link
+        href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/css/bootstrap.min.css"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <p>Contenu de la page</p>
+    </div>
+
+    <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+
+</html>
+
+<body>
+
+</body>
+
+</html>

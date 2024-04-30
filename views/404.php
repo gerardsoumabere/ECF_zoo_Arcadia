cfcc0d8b8fee1daa8b5b9778ed33478f0b21c8c5
gerard_soumabere <gerard.soumabere@gmail.com>
2024-04-30 12:47:54 +0200

    views modifications

--- views/404.php
+++ views/404.php
@@ -1,4 +1,1 @@
 <h1>Erreur 404</h1>
-<p>Vous avez été redirigé vers l'accueil car l'url rentrée dans
-    la barre d'adresse n'existe pas
-    dans la barre</p>
---
+++ views/contact.php
@@ -0,0 +1,1 @@
+<h1>Contact content</h1>
